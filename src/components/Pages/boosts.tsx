f93d94cffbab63086a722bdd2c504e59170ed884
GnurT <trungvv@ftech.ai>
2024-01-17 14:59:49 +0700

    update

--- src/components/Pages/boosts.tsx
+++ src/components/Pages/boosts.tsx
@@ -33,7 +33,7 @@ export const BoostsPage: FC<BoostsPageProps> = ({ count }) => {
         <Balance count={count} />
         <Typography sx={{ marginTop: "10px" }}>Your balance</Typography>
         <Typography variant="h4" sx={{ marginTop: "100px" }}>
-          🚀 {WebApp?.version} 🚀
+          🚀 {WebApp?.initDataUnsafe?.user?.username} 🚀
         </Typography>
         <Typography variant="h4" sx={{ marginTop: "15px" }}>
           Coming soon!
