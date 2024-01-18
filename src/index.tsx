6d6aad861dcb46eb427e4783078faf14b9c11daf
GnurT <trungvv@ftech.ai>
2024-01-18 08:53:51 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ApplicationProvider } from "./contexts/useApp";
+import { Toaster } from "react-hot-toast";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApplicationProvider>
+        <Toaster />
+        <App />
+      </ApplicationProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
