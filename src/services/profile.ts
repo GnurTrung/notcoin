260960463f31b191f8c99792cb7991272acd63ec
GnurT <trungvv@ftech.ai>
2024-01-18 10:08:02 +0700

    update

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -2,7 +2,7 @@ import toast from "react-hot-toast";
 import AxiosInstance from "./api";
 
 export const getUserProfile = async ({ ...params }) => {
-  const url = `${process.env.NEXT_PUBLIC_API_URL}user/get-user`;
+  const url = `https://0bd47518-8000.asse.devtunnels.ms/xaitama-api/user/get-user`;
   toast.error(url);
   return await AxiosInstance.get(url, { params });
 };
